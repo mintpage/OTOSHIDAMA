ab902c3d24cf0039b173869e295e879143dd42f1
desung <desung.mun@icloud.com>
2026-01-04 22:32:18 +0900

    change gift button link

--- OtoshiDama.js
+++ OtoshiDama.js
@@ -1,4 +1,4 @@
-const giftURL = "https://gift.starbucks.co.jp/"; // 本物に差し替え
+const giftURL = "https://gift.starbucks.co.jp/c/1767GcDfZg5ca7om7NjXpTS5A2qhU3Fb"; // 本物に差し替え
 
 // HTMLが読み込まれてから実行
 document.addEventListener("DOMContentLoaded", () => {
